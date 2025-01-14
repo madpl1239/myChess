6a53cd9425b53c329056c6028ed8c95c085f19c8
madpl <madmax1239@gmail.com>
2025-01-14 20:04:12 +0100

    modified chessBoard.cpp/hpp small changes

--- source/chessBoard.cpp
+++ source/chessBoard.cpp
@@ -11,7 +11,7 @@
 
 ChessBoard::ChessBoard()
 {
-	board = std::vector<std::vector<Piece>>(8, std::vector<Piece>(8, Piece()));
+	m_board = std::vector<std::vector<Piece>>(8, std::vector<Piece>(8, Piece()));
 }
 
 
@@ -22,29 +22,29 @@ void ChessBoard::setInitialPositions()
 		for(int x = 0; x < 8; ++x)
 		{
 			if(y == 1)
-				board[y][x] = Piece(PieceType::PAWN, 'W');
+				m_board[y][x] = Piece(PieceType::PAWN, 'W');
 			
 			else if(y == 6)
-				board[y][x] = Piece(PieceType::PAWN, 'B');
+				m_board[y][x] = Piece(PieceType::PAWN, 'B');
 			
 			else if(y == 0 || y == 7)
 			{
 				char color = (y == 0) ? 'W' : 'B';
 				
 				if(x == 0 || x == 7)
-					board[y][x] = Piece(PieceType::ROOK, color);
+					m_board[y][x] = Piece(PieceType::ROOK, color);
 				
 				if(x == 1 || x == 6)
-					board[y][x] = Piece(PieceType::KNIGHT, color);
+					m_board[y][x] = Piece(PieceType::KNIGHT, color);
 				
 				if(x == 2 || x == 5)
-					board[y][x] = Piece(PieceType::BISHOP, color);
+					m_board[y][x] = Piece(PieceType::BISHOP, color);
 				
 				if(x == 3)
-					board[y][x] = Piece(PieceType::QUEEN, color);
+					m_board[y][x] = Piece(PieceType::QUEEN, color);
 				
 				if(x == 4)
-					board[y][x] = Piece(PieceType::KING, color);
+					m_board[y][x] = Piece(PieceType::KING, color);
 			}
 		}
 	}
@@ -53,7 +53,7 @@ void ChessBoard::setInitialPositions()
 
 bool ChessBoard::isPieceAt(int x, int y) const
 {
-	return board[y][x].m_type != PieceType::NONE;
+	return m_board[y][x].m_type != PieceType::NONE;
 }
 
 
@@ -62,10 +62,10 @@ bool ChessBoard::isValidMove(int startX, int startY, int endX, int endY) const
 	if(startX == endX and startY == endY)
 		return false;
 	
-	if(board[startY][startX].m_color == board[endY][endX].m_color)
+	if(m_board[startY][startX].m_color == m_board[endY][endX].m_color)
 		return false;
 
-	Piece piece = board[startY][startX];
+	Piece piece = m_board[startY][startX];
 	int dx = abs(endX - startX);
 	int dy = abs(endY - startY);
 
@@ -102,18 +102,18 @@ bool ChessBoard::validatePawnMove(const Piece& pawn, int startX, int startY, int
 	// Normal move forward
 	if(dx == 0)
 	{
-		if(dy == 1 and board[endY][endX].m_type == PieceType::NONE)
+		if(dy == 1 and m_board[endY][endX].m_type == PieceType::NONE)
 			return startY + direction == endY;
 		
 		if(dy == 2 and ((pawn.m_color == 'W' and startY == 1) or (pawn.m_color == 'B' and startY == 6)))
-			return (board[startY + direction][startX].m_type == PieceType::NONE and
-					board[endY][endX].m_type == PieceType::NONE);
+			return (m_board[startY + direction][startX].m_type == PieceType::NONE and
+					m_board[endY][endX].m_type == PieceType::NONE);
 	}
 
 	// Capture diagonally
 	if(dx == 1 and dy == 1)
-		return (board[endY][endX].m_type != PieceType::NONE and
-				board[endY][endX].m_color != pawn.m_color);
+		return (m_board[endY][endX].m_type != PieceType::NONE and
+				m_board[endY][endX].m_color != pawn.m_color);
 
 	return false;
 }
@@ -129,7 +129,7 @@ bool ChessBoard::validateKingMove(const Piece& king, int startX, int startY, int
 	{
 		int rookX = (endX > startX) ? 7 : 0;
 		
-		if(board[startY][rookX].m_type == PieceType::ROOK and board[startY][rookX].m_color == king.m_color)
+		if(m_board[startY][rookX].m_type == PieceType::ROOK and m_board[startY][rookX].m_color == king.m_color)
 			return isPathClear(startX, startY, rookX, startY);
 	}
 
@@ -147,7 +147,7 @@ bool ChessBoard::isPathClear(int startX, int startY, int endX, int endY) const
 
 	while(x != endX or y != endY)
 	{
-		if(board[y][x].m_type != PieceType::NONE)
+		if(m_board[y][x].m_type != PieceType::NONE)
 			return false;
 		
 		x += xDir;
@@ -166,7 +166,7 @@ bool ChessBoard::isInCheck(char kingColor) const
 	{
 		for(int x = 0; x < 8; ++x)
 		{
-			if(board[y][x].m_type == PieceType::KING and board[y][x].m_color == kingColor)
+			if(m_board[y][x].m_type == PieceType::KING and m_board[y][x].m_color == kingColor)
 			{
 				kingX = x;
 				kingY = y;
@@ -184,7 +184,7 @@ bool ChessBoard::isInCheck(char kingColor) const
 
 	if(kingX == -1 or kingY == -1)
 	{
-		std::cerr << "Error: King not found on board!\n";
+		std::cerr << "Error: King not found on m_board!\n";
 
 		return false;
 	}
@@ -194,11 +194,11 @@ bool ChessBoard::isInCheck(char kingColor) const
 	{
 		for(int x = 0; x < 8; ++x)
 		{
-			if(board[y][x].m_color != kingColor and board[y][x].m_type != PieceType::NONE)
+			if(m_board[y][x].m_color != kingColor and m_board[y][x].m_type != PieceType::NONE)
 			{
 				if(isValidMove(x, y, kingX, kingY))
 				{
-					std::cout << "Piece (" << pieceTypeToString(board[y][x].m_type) 
+					std::cout << "Piece (" << pieceTypeToString(m_board[y][x].m_type) 
 								<< ") at (" << x << ", " << y << ") attack king!\n";
 
 					return true;
@@ -246,10 +246,10 @@ void ChessBoard::movePiece(int startX, int startY, int endX, int endY)
 	std::cout << "\nMoving piece from (" << startX << ", " << startY << ") to (" 
 										<< endX << ", " << endY << ")\n";
 
-	board[endY][endX] = board[startY][startX];
-	board[startY][startX] = Piece();
+	m_board[endY][endX] = m_board[startY][startX];
+	m_board[startY][startX] = Piece();
 
-	char currentPlayerColor = board[endY][endX].m_color;
+	char currentPlayerColor = m_board[endY][endX].m_color;
 	char opponentColor = (currentPlayerColor == 'W') ? 'B' : 'W';
 
 	std::cout << "Checking if " << (opponentColor == 'W' ? "white" : "black") << " king is in check...\n";
@@ -264,16 +264,16 @@ void ChessBoard::movePiece(int startX, int startY, int endX, int endY)
 void ChessBoard::draw(sf::RenderWindow& window, sf::Texture& boardTexture, sf::Texture& figuresTexture, int frameOffset) 
 {
 	sf::Sprite boardSprite(boardTexture);
+	sf::Sprite pieceSprite(figuresTexture);
+	
 	boardSprite.setPosition(0, 0);
 	window.draw(boardSprite);
-
-	sf::Sprite pieceSprite(figuresTexture);
-
+	
 	for(int y = 0; y < 8; ++y) 
 	{
 		for(int x = 0; x < 8; ++x) 
 		{
-			Piece piece = board[y][x];
+			Piece piece = m_board[y][x];
 			if(piece.m_type == PieceType::NONE)
 				continue;
 			
--- source/chessBoard.hpp
+++ source/chessBoard.hpp
@@ -19,13 +19,15 @@ public:
 	bool isPieceAt(int x, int y) const;
 	bool isValidMove(int startX, int startY, int endX, int endY) const;
 	void movePiece(int startX, int startY, int endX, int endY);
+	
+	void draw(sf::RenderWindow& window, sf::Texture& boardTexture, sf::Texture& figuresTexture, int frameOffset);
+	
+private:
 	bool validatePawnMove(const Piece& pawn, int startX, int startY, int endX, int endY, int dx, int dy) const;
 	bool validateKingMove(const Piece& king, int startX, int startY, int endX, int endY, int dx, int dy) const;
 	bool isPathClear(int startX, int startY, int endX, int endY) const;
 	bool isInCheck(char kingColor) const;
 	std::string pieceTypeToString(PieceType type) const;
-	void draw(sf::RenderWindow& window, sf::Texture& boardTexture, sf::Texture& figuresTexture, int frameOffset);
 
-private:
-	std::vector<std::vector<Piece>> board;
+	std::vector<std::vector<Piece>> m_board;
 };
