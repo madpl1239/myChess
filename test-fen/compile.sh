1f4a1415833f6b87cc7ed12b977f35515a35ffd2
madpl <madmax1239@gmail.com>
2025-02-14 19:41:46 +0100

    code reorganisation

--- test-fen/compile.sh
+++ test-fen/compile.sh
@@ -1,3 +1,6 @@
-g++ -std=c++23 -march=native -o testFEN testFEN.cpp ../source/chessBoard.cpp ../source/moveLogger.cpp \
--lgtest -lgtest_main -lpthread -I../source -lsfml-system -lsfml-window \
--lsfml-graphics -lsfml-audio
+# compile.sh for testFEN
+# 12-02-2025 by madpl
+
+g++ -o testFEN testFEN.cpp ../source/chessBoard.cpp ../source/moveLogger.cpp \
+	-lgtest -lgtest_main -lpthread -I../source \
+	-lsfml-system -lsfml-window -lsfml-graphics -std=c++23 -march=native
