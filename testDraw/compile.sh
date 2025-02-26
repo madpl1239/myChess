af21e17bdcc91311a06fc6a0a6fbb5e62c0daadc
madpl1239 <madmax1239@gmail.com>
2025-02-26 19:21:26 +0100

    modified code

--- testDraw/compile.sh
+++ testDraw/compile.sh
@@ -1,6 +1,6 @@
 # compile.sh for testSL
 # 12-02-2025 by madpl
 
-g++ -o testSL saveLoad.cpp ../source/chessBoard.cpp ../source/moveLogger.cpp \
--I../source -lsfml-system -lsfml-window -lsfml-graphics \
--std=c++23 -march=native
+g++ -o testDraw draw.cpp ../source/chessBoard.cpp ../source/moveLogger.cpp \
+../source/sndManager.hpp -I../source -lsfml-system -lsfml-window -lsfml-graphics \
+-lsfml-audio -std=c++23 -march=native
--- testDraw/draw.cpp
+++ testDraw/draw.cpp
@@ -11,7 +11,31 @@
 
 int main(void)
 {
-	chessBoard board();
+	std::cout << "Running test: draw board\n";
+	
+	sf::RenderWindow win(sf::VideoMode(SIZE + 400, SIZE), "");
+	win.setPosition(200, 100);
+	
+	MoveLogger logger(SIZE + 10, 10);
+	SoundManager snd{};
+	
+	ChessBoard board(win, logger, snd);
+	board.setInitialPositions();
+	
+	bool quit = false;
+	while(not quit)
+	{
+		sf::Event e;
+		while(win.poolEvent(e))
+		{
+			if(e.)
+		}
+		
+		board.draw();
+		win.display();
+	}
+	
+	std::cout << "done.\n";
 	
 	return 0;
 }
