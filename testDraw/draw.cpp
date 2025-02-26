5cd5d1d439c5cd770477368bcc3565e53fc47a0f
madpl1239 <madmax1239@gmail.com>
2025-02-26 19:26:49 +0100

    modify code in draw.cpp

--- testDraw/draw.cpp
+++ testDraw/draw.cpp
@@ -19,21 +19,32 @@ int main(void)
 	MoveLogger logger(SIZE + 10, 10);
 	SoundManager snd{};
 	
+	sf::Texture boardTexture;
+	if(not boardTexture.loadFromFile("../resources/board.png"))
+		throw std::runtime_error("Could not load board.png");
+		
+	sf::Texture figuresTexture;
+	if(not figuresTexture.loadFromFile("../resources/figures.png"))
+		throw std::runtime_error("Could not load figures.png");
+	
 	ChessBoard board(win, logger, snd);
 	board.setInitialPositions();
 	
 	bool quit = false;
-	while(not quit)
+	while(win.isOpen() and not quit)
 	{
 		sf::Event e;
 		while(win.poolEvent(e))
 		{
-			if(e.)
+			if(e.event.)
+				guit = true;
 		}
 		
-		board.draw();
+		board.draw(boardTexture, figuresTexture);
 		win.display();
 	}
+	
+	win.close();
 	
 	std::cout << "done.\n";
 	
