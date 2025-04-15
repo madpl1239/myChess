d9cca0c83bb909ef304985fc5f563be918ca0c5f
madPL1239 <madmax1239@gmail.com>
2025-04-15 08:14:22 +0200

    change size of window

--- source/main.cpp
+++ source/main.cpp
@@ -32,7 +32,7 @@ int main(void)
 		std::cout << "myChess testing version\n";
 		#endif
 		
-		sf::RenderWindow window(sf::VideoMode(SIZE + 400, SIZE), "myChess v0.5.0",
+		sf::RenderWindow window(sf::VideoMode(SIZE + SIZE2, SIZE), "myChess v0.5.0",
 								sf::Style::Titlebar | sf::Style::Close);
 		
 		window.setPosition(sf::Vector2i(200, 200));
